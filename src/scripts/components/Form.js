84e28e68c1a3b6ab071699af071e4dbcddcb8d26
Gabriel Stik <gabriel.stik@gmail.com>
2018-03-21 11:40:35 +0100

    :bug: Fixed bug in search form

--- src/scripts/components/Form.js
+++ src/scripts/components/Form.js
@@ -1,14 +1,9 @@
 export default class Form {
 
   constructor() {
-    let place = ''
     const $input = document.querySelector('.input-place .place')
-    $input.addEventListener('input', () => {
-      place = $input.value
-      place = place.replace(' ', '-')
-    })
     $input.addEventListener('keydown', (event) => {
-      if (event.keyCode == 13) window.location.replace(`/${place}`)
+      if (event.keyCode == 13) window.location.replace(`/${ $input.value.replace(' ', '-') }`)
     })
   }
 }
