2cc65a0ddb2a368009d2dfbe575eb203b2d703f4
Gabriel Stik <gabriel.stik@gmail.com>
2018-03-22 11:16:58 +0100

    :sparkles: Working geolocation

--- src/scripts/components/Geolocation.js
+++ src/scripts/components/Geolocation.js
@@ -1,11 +1,18 @@
 export default class Geolocation {
+  constructor() {
+    const $button = document.querySelector('.locate')
+    $button.addEventListener('mousedown', () => {
+      this.getPos()
+    })
+  }
   getPos() {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition((position) =>  {
-        return {
+        const pos = {
           lat: position.coords.latitude,
-          lng: position.coords.longitude
+          long: position.coords.longitude
         }
+        window.location.replace(`/${pos.lat},+${pos.long}`)
       },
       () => { return false },
       { timeout: 10000 })
