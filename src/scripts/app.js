7053c07406a71667fcf87587280e165178b0fea4
Gabriel Stik <gabriel.stik@gmail.com>
2018-03-24 14:13:25 +0100

    :white_check_mark: Working geolocation error catch

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,12 +1,12 @@
 import './vendor/modernizr.js'
 
 import Weather from './components/Weather'
-import Geolocation from './components/Geolocation'
+import Geolocate from './components/Geolocate'
 import Form from './components/Form'
 import Connection from './components/Connection'
 
 const weather = new Weather()
-new Geolocation()
+new Geolocate()
 new Form()
 if (document.querySelector('.js-show-form')) {
   new Connection()
