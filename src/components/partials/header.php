0fecb87d8c3c0944ded6f170ff889c66479c9b11
Gabriel Stik <gabriel.stik@gmail.com>
2018-03-24 14:21:28 +0100

    :lipstick: Fixed styles bugs

--- src/components/partials/header.php
+++ src/components/partials/header.php
@@ -4,7 +4,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="assets/css/reset.min.css">
+  <link rel="stylesheet" href="assets/css/vendor/reset.min.css">
 	<link rel="stylesheet" href="assets/css/style.min.css">
 	<link rel="stylesheet" href="assets/lib/font-awesome-4.7.0/css/font-awesome.min.css">
 	<title>Météo</title>
